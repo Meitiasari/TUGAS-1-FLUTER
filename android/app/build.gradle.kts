fb6db9d387de26610bb9b42a331ab70d8385345f
Meitiasari <memey.icloud@gmail.com>
2025-02-21 14:38:45 +0700

    Update fitur baru

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,11 @@ plugins {
 }
 
 android {
+<<<<<<< HEAD
     namespace = "com.example.flutterapp"
+=======
+    namespace = "com.example.meiapp"
+>>>>>>> 5ea17bf (commit)
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +25,11 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+<<<<<<< HEAD
         applicationId = "com.example.flutterapp"
+=======
+        applicationId = "com.example.meiapp"
+>>>>>>> 5ea17bf (commit)
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+<<<<<<< HEAD
 import 'state_screen.dart'; // Pastikan import sesuai dengan path proyek
 
 void main() {
   runApp(MainApp()); // HAPUS 'const'
+=======
+import 'package:meiapp/screens/dynamic_list_screen.dart';
+import 'package:meiapp/screens/products/product_list_screen.dart';
+
+void main() {
+  runApp(const MainApp());
+>>>>>>> 5ea17bf (commit)
 }
 
 class MainApp extends StatelessWidget {
@@ -10,8 +18,18 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+<<<<<<< HEAD
     return MaterialApp( // HAPUS 'const'
       home: StateScreen(),
     );
   }
 }
+=======
+    return const MaterialApp(
+      home: Scaffold(
+        body: ProductListScreen()
+      ),
+    );
+  }
+}
+>>>>>>> 5ea17bf (commit)
